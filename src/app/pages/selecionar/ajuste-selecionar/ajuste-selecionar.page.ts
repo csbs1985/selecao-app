8626cf5cd4b40d8f1d8dd7f13c0f566ffd2442e7
Charles Santos <csbs.conta@outlook.com>
2021-04-23 16:14:43 -0300

    add input

--- src/app/pages/selecionar/ajuste-selecionar/ajuste-selecionar.page.ts
+++ src/app/pages/selecionar/ajuste-selecionar/ajuste-selecionar.page.ts
@@ -9,6 +9,22 @@ export class AjusteSelecionarPage implements OnInit {
   readonly textoCabecalho = 'Ajustar placa e cronômetro';
   readonly textoBotao = 'Confirmar';
 
+  periodosArray = {
+    name: 'periodo',
+    checked: 2,
+    valor: [
+      1, 2
+    ]
+  }
+
+  duracaoArray = {
+    name: 'duracao',
+    checked: 45,
+      valor: [
+        10, 15, 20, 25, 30, 35, 40, 45
+      ]
+  }
+
   constructor() { }
 
   ngOnInit() { }
