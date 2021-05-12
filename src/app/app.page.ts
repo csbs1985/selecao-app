e7fcadefe2d462f0beb07fa04049be1ef624e20b
Charles Santos <csbs.conta@outlook.com>
2021-05-12 09:53:07 -0300

    nada de mais

--- src/app/app.page.ts
+++ src/app/app.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { Router } from '@angular/router';
+import { Plugins } from '@capacitor/core';
+
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -13,7 +16,9 @@ export class AppPage implements OnInit {
     private statusBar: StatusBar,
     private screenOrientation: ScreenOrientation,
     private route: Router
-  ) { }
+  ) {
+    SplashScreen.hide();
+  }
 
   ngOnInit() {
     this.statusBar.backgroundColorByHexString('#2A2A2A');
