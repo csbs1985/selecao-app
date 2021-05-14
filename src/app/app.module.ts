dab2220fbf09b53725f5f7d7ee17f710e3557095
Charles Santos <csbs.conta@outlook.com>
2021-05-14 16:32:26 -0300

    nem sei o que fiz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { ComponentsModule } from './components/components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Vibration } from '@ionic-native/vibration/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @NgModule({
   declarations: [AppPage],
@@ -32,6 +33,7 @@ import { Vibration } from '@ionic-native/vibration/ngx';
     StatusBar,
     ScreenOrientation,
     Vibration,
+    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppPage],
--- src/app/app.page.ts
+++ src/app/app.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +14,22 @@ export class AppPage {
   constructor(
     private statusBar: StatusBar,
     private screenOrientation: ScreenOrientation,
-    private route: Router
+    private router: Router,
+    private platform: Platform,
+    private splashScreen: SplashScreen
   ) {
     this.iniciarApp();
   }
 
   iniciarApp() {
+    this.platform.ready().then(() => {
+      setTimeout(() => {
+        this.splashScreen.hide();
+        this.router.navigate(['/inicio']);
+      }, 1000);
+    });
+
     this.statusBar.backgroundColorByHexString('#2A2A2A');
     this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
-    this.route.navigateByUrl('splash');
   }
 }
