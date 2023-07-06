fdffe2c78149e779622e219ce9f093473263e983
Charles santos <charles.batista@fleetcor.com.br>
2023-07-06 16:14:37 -0300

    [SegundoButton] criar

---
+++ lib/button/segundo_button.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SegundoButton extends StatefulWidget {
+  const SegundoButton({
+    super.key,
+    required Function callback,
+    required Color cor,
+    bool? duplo,
+    IconData? icone,
+    required double size,
+  })  : _callback = callback,
+        _cor = cor,
+        _duplo = duplo,
+        _icone = icone,
+        _size = size;
+
+  final Function _callback;
+  final Color _cor;
+  final bool? _duplo;
+  final IconData? _icone;
+  final double _size;
+
+  @override
+  State<SegundoButton> createState() => _ItemMenuWidgetState();
+}
+
+class _ItemMenuWidgetState extends State<SegundoButton> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) => setState(() => isPressed = true),
+      onTapUp: (_) => setState(() => isPressed = false),
+      onTapCancel: () => setState(() => isPressed = false),
+      onTap: () => widget._callback(),
+      child: Container(
+        width: widget._duplo == true ? (widget._size * 2) : widget._size,
+        height: widget._size,
+        color: isPressed ? const Color(0xFF0C2D54) : widget._cor,
+        child: Center(
+          child: Icon(
+            widget._icone,
+            size: widget._size * 0.5,
+            color: isPressed ? Colors.white : const Color(0xFF0C2D54),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/page/placar_page.dart
+++ lib/page/placar_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:selecao_app/appbar/padrao_appbar.dart';
+import 'package:selecao_app/button/segundo_button.dart';
 
 class PlacarPage extends StatefulWidget {
   const PlacarPage({super.key});
@@ -11,9 +12,87 @@ class PlacarPage extends StatefulWidget {
 class _PlacarPageState extends State<PlacarPage> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final size = width / 6;
+
     return Scaffold(
       appBar: PadraoAppbar(callback: () => {}),
-      body: const SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFF1CF559),
+                  icone: Icons.timer,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xffC8B7F3),
+                  icone: Icons.play_arrow,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xffC8B7F3),
+                  icone: Icons.pause,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xffC8B7F3),
+                  icone: Icons.stop,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFFFFFFFF),
+                  icone: Icons.scoreboard,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xffC8F51B),
+                  icone: Icons.settings,
+                  size: size,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFFFCD45C),
+                  duplo: true,
+                  icone: Icons.plus_one,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFFFCD45C),
+                  icone: Icons.remove,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFFDA714F),
+                  icone: Icons.remove,
+                  size: size,
+                ),
+                SegundoButton(
+                  callback: () => {},
+                  cor: const Color(0xFFDA714F),
+                  duplo: true,
+                  icone: Icons.plus_one,
+                  size: size,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
